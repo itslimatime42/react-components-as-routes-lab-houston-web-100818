9366be029afc97b37de0b1952f9dc9ea3efa48ae
Robert Schulte <schulte.robertw@outlook.com>
2018-12-17 17:19:02 -0600

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const displayDirector = (director) => {
+    return (
+      <div>
+        <h2>Name: {director.name}</h2>
+        <p>Movies: </p>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => displayDirector(director))}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const displayMovie = (movie) => {
+    return (
+      <div>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <p>Genres: </p>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => displayMovie(movie))}
     </div>
   );
 };
